98900be198abba590efa3a68fa900e996533cffb
lennonpetrick <lennonpetrick@gmail.com>
2019-02-22 11:03:32 -0300

    Created layouts for Movies screen, movie detail, character search and character detail

--- app/src/main/java/com/test/starwarsapp/CharacterSearchActivity.kt
+++ app/src/main/java/com/test/starwarsapp/CharacterSearchActivity.kt
@@ -3,10 +3,11 @@ package com.test.starwarsapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity() {
+class CharacterSearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_character_search)
     }
+
 }
